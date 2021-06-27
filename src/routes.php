0299b387929a6fedf80cd3779ab90277f69d3364
Rollswan Acebedo <rollswan.acebedo@summitmedia.com.ph>
2021-06-27 19:54:35 +0800

    fix bug

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/updater', 'namespace' => 'Rollswan\LaravelProjectUpdater\Controllers'], function () {
-    Route::post('/', 'UpdaterController@updateProject')->middleware('laravel-project-updater');
+    Route::post('/', 'UpdaterController@updateProject')->middleware('lpu');
 });
